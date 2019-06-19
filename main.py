8804d0186f60693e98185772a8c1f0c6ff1383c4
EliasKassapis <Kassapiselias@hotmail.co.uk>
2019-06-19 21:29:50 +0200

    weights to triple-paper

--- main.py
+++ main.py
@@ -228,7 +228,7 @@ def parse():
     parser.add_argument('--n_hidden', type=int, default=24, help='features in the first hidden layer')
 
     # optimizer arguments
-    parser.add_argument('--discriminator_optimizer', default="SGD", type=str, help="name of objectclass")
+    parser.add_argument('--discriminator_optimizer', default="Adam", type=str, help="name of objectclass")
     parser.add_argument('--generator_optimizer', default="Adam", type=str, help="name of objectclass")
     parser.add_argument('--embedder_optimizer', default="Adam", type=str, help="name of objectclass")
 
@@ -240,13 +240,13 @@ def parse():
     # hyperparams generatorloss  (-1 === DEFAULT)
     parser.add_argument('--NonSaturatingGLoss_weight', default=1, type=float,
                         help="weight hyperparameter for specific generatorloss")
-    parser.add_argument('--PixelLoss_weight', default=4000, type=float,
+    parser.add_argument('--PixelLoss_weight', default=10, type=float,
                         help="weight hyperparameter for specific generatorloss")
-    parser.add_argument('--PerceptualLoss_weight', default= 1, type=float,
+    parser.add_argument('--PerceptualLoss_weight', default= 10, type=float,
                         help="weight hyperparameter for specific generatorloss")
     parser.add_argument('--ConsistencyLoss_weight', default=100, type=float,
                         help="weight hyperparameter for specific generatorloss")
-    parser.add_argument('--TripleConsistencyLoss_weight', default=1000, type=float,
+    parser.add_argument('--TripleConsistencyLoss_weight', default=100, type=float,
                         help="weight hyperparameter for specific generatorloss")
     parser.add_argument('--IdLoss_weight', default=1, type=float,
                         help="weight hyperparameter for specific generatorloss")
