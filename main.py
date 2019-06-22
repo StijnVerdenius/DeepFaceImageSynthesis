8a4620bfac0fea201d67baad5d2c173837413061
EliasKassapis <Kassapiselias@hotmail.co.uk>
2019-06-23 00:14:24 +0200

    removed randomcrop

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def load_data(keyword: str, batch_size: int, mode: str, n_videos_limit: Optional
     transform = transforms.Compose(
         [
             transformations.RandomHorizontalFlip(),
-            transformations.RandomCrop(probability=0.5),
+            # transformations.RandomCrop(probability=0.5),
             transformations.Resize(),
             transformations.RescaleValues(),
             transformations.ChangeChannels(),
